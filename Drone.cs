492e0f96a603f9e44e62d5b0945e36b989ef9088
Ryan Koym <RyanKoym@gmail.com>
2012-03-05 01:07:04 -0600

    New Upgrades: Seeker Rocket and Drones
     - New upgrade rocket that tracks the boss
     - Drones, such as automated turrets, wingmen,
       or shields usable by either player or boss, may
       be set to stationary, or to stay by owner's side or
       orbit owner.
    
    (Seeker Rockets never miss, so they may be "too" perfect as they are now)
    (Drones are just a shell to be used for specific upgrades later)
    (Neither are implemented, just waiting to integrate with the new upgrade system)

---
+++ Drone.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hook.Graphics;
+using Hook.Graphics.SpriteSheets;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace teamstairwell
+{
+    public class Drone : Entity
+    {
+
+        //Specifies the movement pattern of the drone
+        public enum droneType
+        {
+            Fixed,
+            Orbital,
+            SideBySide
+        }
+
+        public int health;
+
+        List<Bullet> dBs;           //Drone bullet list
+        Entity master;              //Reference to controlling unit
+        droneType type;
+
+        Sprite normalSprite;
+
+        public Drone(Vector2 position, Vector2 velocity, Vector2 acceleration, float radius, droneType type, Entity master)
+            : base(position, velocity, acceleration, radius)
+        {
+            this.type = type;
+            this.master = master;
+            dBs = new List<Bullet>();
+
+            normalSprite = new Sprite(RNSEB.PlayerSheet, PlayerSheet.PNORMAL);
+        }
+
+        public override void update(GameTime time)
+        {
+            if (type == droneType.SideBySide)
+            {
+                rot = master.rot;
+                pos.X = master.pos.X + (float)Math.Cos(rot) * 50;
+                pos.Y = master.pos.Y + (float)Math.Sin(rot) * 50;
+            }
+            if (type == droneType.Orbital)
+            {
+                rot += (float)(time.ElapsedGameTime.Milliseconds) / 1000;
+                pos.X = master.pos.X + (float)Math.Cos(rot) * 50;
+                pos.Y = master.pos.Y + (float)Math.Sin(rot) * 50;
+            }
+
+            base.update(time);
+        }
+    }
+}
